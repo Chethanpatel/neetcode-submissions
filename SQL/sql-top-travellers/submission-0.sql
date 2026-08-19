1e5c36cd6eb5b0b165ff4fa5d086608a4038d394
CHETHAN PATEL <87313707+Chethanpatel@users.noreply.github.com>
2026-08-19 23:21:27 +0530

    Add: sql-top-travellers - submission-0

---
+++ SQL/sql-top-travellers/submission-0.sql
@@ -0,0 +1,9 @@
+-- Write your query below
+
+select name, travelled_distance
+from (
+select u.id , name, COALESCE(sum(distance), 0) as travelled_distance
+from  users u left join rides r on u.id = r.user_id
+group by u.id, name
+)
+order by 2 desc, 1 asc
